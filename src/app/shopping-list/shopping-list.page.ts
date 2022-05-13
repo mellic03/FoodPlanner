0cb08e2206e7df48250bbafd3eb2eb0dcc9f3385
Michael Ellicott <mellic03@gmail.com>
2022-05-13 10:21:06 +1000

    Shopping list now sorted alphabetically

--- src/app/shopping-list/shopping-list.page.ts
+++ src/app/shopping-list/shopping-list.page.ts
@@ -14,7 +14,7 @@ export class ShoppingListPage implements OnInit {
     // loads the stored recipes
     this.storage.get("persistent_recipes").then( val => {
       this.persistent_recipes = val;
-      this.shopping_list = this.storage.getAllInfo(val);
+      this.shopping_list = this.sortShoppingList(this.storage.getAllInfo(val));
     });
   }
 
@@ -24,6 +24,23 @@ export class ShoppingListPage implements OnInit {
     this.storage.checkIngredient(ingredient_name, newValue);
   }
 
+  // Return a shopping list in alphabetical order
+  sortShoppingList(shopping_list:Array<Array<any>>) {
+    let sorted_shopping_list = shopping_list;
+    for (let i = 0; i < sorted_shopping_list.length-1; i++) {
+      for (let j = 0; j < sorted_shopping_list.length-1; j++) {
+        if (sorted_shopping_list[j][0].substring(0, 1) > sorted_shopping_list[j+1][0].substring(0, 1)) {
+          let temp_obj:Array<any> = sorted_shopping_list[j];
+          sorted_shopping_list[j] = sorted_shopping_list[j+1];
+          sorted_shopping_list[j+1] = temp_obj;
+        }
+      }
+    }
+    return(sorted_shopping_list);
+  }
+
   persistent_recipes;
   shopping_list = [];
+
+  // Two shopping lists,
 }
